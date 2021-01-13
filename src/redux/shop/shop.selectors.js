dc0781d3823984e3a0c8b826357cade6b1571b48
Saad Muhamamd <s.muhammad86@gmail.com>
2021-01-13 22:25:28 +0500

    bug fixing

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,14 +1,19 @@
-import { createSelector } from "reselect";
+import { createSelector } from 'reselect';
 
-const selectShop = (state) => state.shop;
+const selectShop = state => state.shop;
 
-export const selectShopCollections = createSelector(
+export const selectCollections = createSelector(
   [selectShop],
-  (shop) => shop.collections
+  shop => shop.collections
 );
 
-export const selectCollection = (CollectionUrlParam) =>
-  createSelector([selectShopCollections],
-    collections => collections(CollectionUrlParam)
-    )
+export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
+  collections => Object.keys(collections).map(key => collections[key])
+);
 
+export const selectCollection = collectionUrlParam =>
+  createSelector(
+    [selectCollections],
+    collections => collections[collectionUrlParam]
+  );
